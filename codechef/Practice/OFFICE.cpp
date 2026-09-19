4fa285b3f8788048b48551e8d228c86c4920c716
Sandhipan Das <sandhipandascodes@gmail.com>
2026-09-19 19:55:15 +0530

    Solved: OFFICE (codechef)

---
+++ codechef/Practice/OFFICE.cpp
@@ -0,0 +1,21 @@
+// Problem: OFFICE
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/OFFICE
+// Solved on: 2026-09-19T14:25:13.728Z
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin>>T;
+	
+	while(T--){
+    int X,Y;
+    cin>>X >> Y;
+    cout << 4*X+Y  << endl;
+}
+  return 0;  
+}
