87fea88361902a7bd209aac8fcc7a6e1841b57cf
Sandhipan Das <sandhipandascodes@gmail.com>
2026-09-20 22:44:24 +0530

    Solved: CREDCOINS (codechef)

---
+++ codechef/Practice/CREDCOINS.cpp
@@ -0,0 +1,25 @@
+// Problem: CREDCOINS
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/CREDCOINS
+// Solved on: 2026-09-20T17:14:22.847Z
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin>>T;
+	
+	
+	while(T--){
+	int X,Y;
+	cin>> X >> Y ;
+	
+	
+	cout<< ((X*Y)/100)<<endl;
+	}
+
+return 0;
+}
