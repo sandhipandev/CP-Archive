2c88767fe1adc326b9bad3ff655d6ce0eb30cc94
Sandhipan Das <sandhipandascodes@gmail.com>
2026-09-21 18:05:27 +0530

    Delete codechef/Practice/AVGPROBLEM.cpp

--- codechef/Practice/AVGPROBLEM.cpp
+++
@@ -1,8 +0,0 @@
-// Problem: Explained Solutions
-// Platform: codechef
-// Language: C++​
-// Verdict: Accepted
-// URL: https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/AVGPROBLEM?tab=Submissions
-// Solved on: 2026-09-21T12:25:08.398Z
-
-// source not captured automatically - open the solution page and copy it manually
