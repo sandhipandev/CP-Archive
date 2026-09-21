5245f570a8cd1880281a6aa8614d532987ac0207
Sandhipan Das <sandhipandascodes@gmail.com>
2026-09-21 17:54:09 +0530

    Solved: Explained Solutions (codechef)

--- codechef/Practice/AVGPROBLEM.cpp
+++ codechef/Practice/AVGPROBLEM.cpp
@@ -3,6 +3,6 @@
 // Language: C++​
 // Verdict: Accepted
 // URL: https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/AVGPROBLEM?tab=Submissions
-// Solved on: 2026-09-21T12:24:05.865Z
+// Solved on: 2026-09-21T12:24:07.259Z
 
 // source not captured automatically - open the solution page and copy it manually
