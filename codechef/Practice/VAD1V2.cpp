07561a6056b478448b1b4a14ad019e4e8a24c0bb
Sandhipan Das <sandhipandascodes@gmail.com>
2026-09-22 11:57:48 +0530

    Solved: VAD1V2 (codechef)

---
+++ codechef/Practice/VAD1V2.cpp
@@ -0,0 +1,15 @@
+// Problem: VAD1V2
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/learn/course/cpp/LTCCPP05/problems/VAD1V2
+// Solved on: 2026-09-22T06:27:46.838Z
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Replace __ with 25
+    int age = 25;
+    cout << age;
+}
