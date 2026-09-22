3f5b29313a87aa97d72a38f0bff15bcad280a200
Sandhipan Das <sandhipandascodes@gmail.com>
2026-09-23 01:52:22 +0530

    Solved: LCPPCL27 (codechef)

---
+++ codechef/Practice/LCPPCL27.c
@@ -0,0 +1,22 @@
+// Problem: LCPPCL27
+// Platform: codechef
+// Language: int num = 100000;
+   long long large_num = 100000000000000;
+   float pi = 3.14;
+   double euler_number = 2.718281828459045;
+   char c = 'A';
+   bool check = true;
+// Verdict: Accepted
+// URL: https://www.codechef.com/learn/course/cpp/LTCCPP05/problems/LCPPCL27
+// Solved on: 2026-09-22T20:22:20.536Z
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Declare a variable
+   int number =19;
+    
+    // Print the variable
+    cout << number;
+}
