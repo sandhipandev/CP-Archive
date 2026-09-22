136c215c29a9044a57b5bce6c0d736a3d4c2f63b
Sandhipan Das <sandhipandascodes@gmail.com>
2026-09-23 01:52:59 +0530

    Solved: SYNMCQ7 (codechef)

---
+++ codechef/Practice/SYNMCQ7.c
@@ -0,0 +1,19 @@
+// Problem: SYNMCQ7
+// Platform: codechef
+// Language: #include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int x = 3;
+    int y = 1;
+    cout << x + y;
+    
+    return 0;
+}
+// Verdict: Accepted
+// URL: https://www.codechef.com/learn/course/cpp/LTCCPP06/problems/SYNMCQ7
+// Solved on: 2026-09-22T20:22:58.650Z
+
+// source not captured automatically - copy it from the editor and use Manual Push
