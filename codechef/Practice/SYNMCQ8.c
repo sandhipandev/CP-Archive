b3dd073f3ec671913d81cd9ff259a77406f461a0
Sandhipan Das <sandhipandascodes@gmail.com>
2026-09-23 01:53:55 +0530

    Solved: SYNMCQ8 (codechef)

---
+++ codechef/Practice/SYNMCQ8.c
@@ -0,0 +1,17 @@
+// Problem: SYNMCQ8
+// Platform: codechef
+// Language: #include <iostream>
+using namespace std;
+
+int main() {
+
+  int a = 3 ;      
+  int b = 1 ;      
+  cout << x + y ;   
+
+}
+// Verdict: Accepted
+// URL: https://www.codechef.com/learn/course/cpp/LTCCPP06/problems/SYNMCQ8
+// Solved on: 2026-09-22T20:23:54.299Z
+
+// source not captured automatically - copy it from the editor and use Manual Push
