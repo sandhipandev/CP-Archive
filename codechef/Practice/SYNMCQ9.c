fc77e305189ac5c93b5819161158f15a77145d2b
Sandhipan Das <sandhipandascodes@gmail.com>
2026-09-23 01:54:19 +0530

    Solved: SYNMCQ9 (codechef)

---
+++ codechef/Practice/SYNMCQ9.c
@@ -0,0 +1,17 @@
+// Problem: SYNMCQ9
+// Platform: codechef
+// Language: #include <iostream>
+using namespace std;
+
+int main() {
+  int x = 5 ;
+  int y = 2 ;
+
+  int sum = x + y ;
+  cout << sum ; 
+}
+// Verdict: Accepted
+// URL: https://www.codechef.com/learn/course/cpp/LTCCPP06/problems/SYNMCQ9
+// Solved on: 2026-09-22T20:24:18.227Z
+
+// source not captured automatically - copy it from the editor and use Manual Push
