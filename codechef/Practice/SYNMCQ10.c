d0a860a7d6c61fcc08f8ff5c3cc634fc412b5589
Sandhipan Das <sandhipandascodes@gmail.com>
2026-09-24 01:39:52 +0530

    Solved: SYNMCQ10 (codechef)

---
+++ codechef/Practice/SYNMCQ10.c
@@ -0,0 +1,17 @@
+// Problem: SYNMCQ10
+// Platform: codechef
+// Language: #include <iostream>
+using namespace std;
+
+int main() {
+  int a = 10 ;
+  int b = 3 ;
+  int c = a / b;
+  cout << c;
+  
+}
+// Verdict: Accepted
+// URL: https://www.codechef.com/learn/course/cpp/LTCCPP07/problems/SYNMCQ10
+// Solved on: 2026-09-23T20:09:50.222Z
+
+// source not captured automatically - copy it from the editor and use Manual Push
