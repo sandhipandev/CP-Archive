68fbf60ea7bf8868e567d6f5e11a124b9a63ba77
Sandhipan Das <sandhipandascodes@gmail.com>
2026-09-24 01:42:15 +0530

    Solved: SYNMCQ12 (codechef)

---
+++ codechef/Practice/SYNMCQ12.cpp
@@ -0,0 +1,17 @@
+// Problem: SYNMCQ12
+// Platform: codechef
+// Language: #include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int a = 10 ;
+  int b = 3 ;
+  cout << "a / b";
+  
+  
+}
+// Verdict: Accepted
+// URL: https://www.codechef.com/learn/course/cpp/LTCCPP07/problems/SYNMCQ12
+// Solved on: 2026-09-23T20:12:13.960Z
+
+// source not captured automatically - copy it from the editor and use Manual Push
