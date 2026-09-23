b209cd365e54e0595dd5ce8069e9a6fb800bdcf0
Sandhipan Das <sandhipandascodes@gmail.com>
2026-09-24 01:41:54 +0530

    Solved: SYNMCQ11 (codechef)

---
+++ codechef/Practice/SYNMCQ11.cpp
@@ -0,0 +1,17 @@
+// Problem: SYNMCQ11
+// Platform: codechef
+// Language: #include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int a = 10 ;
+  int b = 3 ;
+  cout << a / b;
+  
+  
+}
+// Verdict: Accepted
+// URL: https://www.codechef.com/learn/course/cpp/LTCCPP07/problems/SYNMCQ11
+// Solved on: 2026-09-23T20:11:53.669Z
+
+// source not captured automatically - copy it from the editor and use Manual Push
