0e8bacf5a18e83c7804da789ba10aad36be0d977
Sandhipan Das <sandhipandascodes@gmail.com>
2026-09-25 01:18:13 +0530

    Solved: VAD9V2 (codechef)

---
+++ codechef/Practice/VAD9V2.cpp
@@ -0,0 +1,31 @@
+// Problem: VAD9V2
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/learn/course/cpp/LTCCPP07/problems/VAD9V2
+// Solved on: 2026-09-24T19:48:09.756Z
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int a = -50;
+    int b = 40;
+    // Update your code below this line
+ int sum=a+b;
+ int product=a*b;
+ int quotient=a/b;
+ cout<<sum<<endl;
+cout<<product<<endl;
+cout<<quotient<<endl;
+return 0;
+}
+
+
+
+
+
+
+  
+  
