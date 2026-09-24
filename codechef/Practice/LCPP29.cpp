6800367669f37931c88e53b3ee6888e21cbbda83
Sandhipan Das <sandhipandascodes@gmail.com>
2026-09-25 01:25:53 +0530

    Solved: Identify correct syntax (codechef)

---
+++ codechef/Practice/LCPP29.cpp
@@ -0,0 +1,17 @@
+// Problem: Identify correct syntax
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/skill-test/LCPPMOD2/problems/LCPP29
+// Solved on: 2026-09-24T19:55:50.107Z
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+     int x=20;
+     int y=6;
+     cout<<x/y;
+
+    return 0;
+}
