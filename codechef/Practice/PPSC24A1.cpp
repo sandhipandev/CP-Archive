6db19132486c1331678c1419e467ab871b991071
Sandhipan Das <sandhipandascodes@gmail.com>
2026-09-26 20:19:54 +0530

    Solved: PPSC24A1 (codechef)

---
+++ codechef/Practice/PPSC24A1.cpp
@@ -0,0 +1,17 @@
+// Problem: PPSC24A1
+// Platform: codechef
+// Language: int a = 6;
+int b = 3;
+// Verdict: Accepted
+// URL: https://www.codechef.com/learn/course/cpp/LTCCPP07C/problems/PPSC24A1
+// Solved on: 2026-09-26T14:49:53.380Z
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+  cout <<  21 + 40; //Replace the _ with the correct value
+
+  return 0;
+}
