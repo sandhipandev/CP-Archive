31928c067bdebdc773063642a55f0226243b4faa
Sandhipan Das <sandhipandascodes@gmail.com>
2026-09-26 20:26:25 +0530

    Solved: CPPCLOP03 (codechef)

---
+++ codechef/Practice/CPPCLOP03.c
@@ -0,0 +1,10 @@
+// Problem: CPPCLOP03
+// Platform: codechef
+// Language: int x = 7;
+x *= 3;
+cout << x;
+// Verdict: Accepted
+// URL: https://www.codechef.com/learn/course/cpp/LTCCPP07C/problems/CPPCLOP03
+// Solved on: 2026-09-26T14:56:24.615Z
+
+// source not captured automatically - copy it from the editor and use Manual Push
