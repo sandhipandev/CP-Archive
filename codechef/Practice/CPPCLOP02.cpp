483aff87c42b3ffe8c622c3674e0898f11f35c24
Sandhipan Das <sandhipandascodes@gmail.com>
2026-09-26 20:25:48 +0530

    Solved: CPPCLOP02 (codechef)

---
+++ codechef/Practice/CPPCLOP02.cpp
@@ -0,0 +1,18 @@
+// Problem: CPPCLOP02
+// Platform: codechef
+// Language: int length;
+length = 15;
+// Verdict: Accepted
+// URL: https://www.codechef.com/learn/course/cpp/LTCCPP07C/problems/CPPCLOP02
+// Solved on: 2026-09-26T14:55:47.442Z
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int length = 11;
+  cout<< length%3<<endl;
+  // Update your code below this line
+  
+  
+}
