26d3b7913c2ca1c890860c5610329a38ba8008f2
Sandhipan Das <sandhipandascodes@gmail.com>
2026-09-26 20:27:17 +0530

    Solved: CPPCLOP04 (codechef)

---
+++ codechef/Practice/CPPCLOP04.c
@@ -0,0 +1,11 @@
+// Problem: CPPCLOP04
+// Platform: codechef
+// Language: int a = 10;
+int b = 2;
+a /= b;
+cout<<a;
+// Verdict: Accepted
+// URL: https://www.codechef.com/learn/course/cpp/LTCCPP07C/problems/CPPCLOP04
+// Solved on: 2026-09-26T14:57:16.222Z
+
+// source not captured automatically - copy it from the editor and use Manual Push
